e5b8be30b7e6ccbe0e8c45dec70c18cf0a0dab13
sonnguyenece <sonnguyenece@gmail.com>
2020-04-09 21:35:11 +0700

    add

--- src/IllegalException.java
+++ src/IllegalException.java
@@ -10,7 +10,7 @@ public class IllegalException {
         System.out.print("Input side 1 : ");
         float side3 = scn.nextFloat();
         try {
-            IllegalTriangleException(side1, side2, side3);
+            checkTriangle(side1, side2, side3);
             System.out.println("Its a triangle!");
         } catch (Exception e) {
             System.out.println("Wrong input " + e.getMessage());
@@ -25,9 +25,14 @@ public class IllegalException {
 
     }
 
-    static void IllegalTriangleException(float side1, float side2, float side3) throws Exception {
+    static void checkTriangle(float side1, float side2, float side3) throws IllegalTriangleException {
         if (side1 <= 0 || side2 <= 0 || side3 <= 0
                 || (side1 + side2 < side3) || (side1 + side3 < side2) || (side2 + side3 < side1)
-        ) throw new Exception();
+        ) throw new IllegalTriangleException(" valid ");
+    }
+    static class IllegalTriangleException  extends Exception {
+        IllegalTriangleException (String s) {
+            super(s);
+        }
     }
 }
